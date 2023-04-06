a35a8e389f9b75e0565918a42f2a8306a709de4c
andrew21-mch <nfonandrew73@gmail.com>
2023-04-06 14:55:57 +0100

    resulabl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-        <style>
+        {{-- font awesome --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
         body {
             font-family: 'figtree', cursive;
         }
@@ -22,12 +24,7 @@
         .row {
             /* animate */
             animation: animate 1s;
-
-            /* transition */
             transition: all 1s;
-
-
-
         }
 
         @keyframes animate {
@@ -45,13 +42,29 @@
         .row:hover {
             transform: scale(1.05);
         }
-        </style>
+
+        @media (max-width: 768px) {
+            .row {
+                transform: scale(1);
+            }
+        }
+
+        /* general scroll speed */
+        html {
+            scroll-behavior: smooth;
+        }
+
+
+        .btn-primary {
+            background-color: #0d6efd;
+            border-color: #0d6efd;
+        }
+    </style>
 
 </head>
 
 <body class="antialiased">
-    {{-- navigation --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
         <a class="navbar-brand" href="#">Resulab</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,10 +74,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Docs</a>
+                    <a class="nav-link" href="#docs">Docs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="#contact">Contact</a>
                 </li>
 
             </ul>
@@ -74,26 +87,27 @@
             </form>
         </div>
     </nav>
-    <div class="container">
+    <div class="container mt-9">
         <div class="row m-5">
             <div class="col-md-6">
                 <h1 class="display-4">Resulab</h1>
                 <p class="lead">
-                    This is a simple api to generate resume online.
+                    We prepare you for your next interview,
+                    we have a well documented api. follow the link to get started.
                 </p>
-                <a href="#" class="btn btn-primary">Get Started</a>
+                <a href="https://interstellar-robot-966971.postman.co/workspace/My-Workspace~73d79d56-8070-49a0-8d8d-92ae5e5d14b2/api/9e3c345b-13c4-43d0-b7c7-ad6532a02679" class="btn btn-primary">Get Started</a>
             </div>
             <div class="col-md-6">
-                <img src="https://images.unsplash.com/photo-1586281380349-632531db7ed4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdW1lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                <img src="https://images.unsplash.com/photo-1586281380349-632531db7ed4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdW1lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop"
                     alt="" class="img-fluid img-thumbnail">
             </div>
         </div>
         {{-- docs --}}
-        <div class="row m-5">
+        <div class="row m-5" id="docs">
             <div class="col-md-6">
                 {{-- svg for docs --}}
-                <img src="https://images.unsplash.com/photo-1600267204091-5c1ab8b10c02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                    alt="" class="img-fluid">
+                <img src="https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="" class="img-fluid img-thumbnail">
 
             </div>
             <div class="col-md-6">
@@ -102,37 +116,158 @@
                     {{-- some description of the documenation --}}
                     We have a well documented api. follow the link to get started.
                 </p>
-                <a href="https://documenter.getpostman.com/view/13220146/Tz5qZz8m" class="btn btn-primary">View
+                <a href="https://interstellar-robot-966971.postman.co/workspace/My-Workspace~73d79d56-8070-49a0-8d8d-92ae5e5d14b2/api/9e3c345b-13c4-43d0-b7c7-ad6532a02679" class="btn btn-primary" target="_blank">
                     Documenation</a>
             </div>
         </div>
+    </div>
+    {{-- our team --}}
+    {{-- row of team mebmers --}}
+    <div class="team container-fluid bg-light" id="contact">
+        <h2 class="text-center m-auto mt-5">Our Team</h2>
+        <hr class="w-25 mx-auto">
+        <div class="row m-5">
+            <div class="col-md-3">
+                <img src="https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="" class="img-fluid img-thumbnail">
+                <h3 class="text-center">John Doe</h3>
+                <p class="text-center">CEO</p>
 
-        <div class="row">
-            <div class="col-md-6">
-                <h1 class="display-4">Contact</h1>
-                <p class="lead">
-                    follow the link to get started.
-                </p>
-                <a href="https://documenter.getpostman.com/view/13220146/Tz5qZz8m" class="btn btn-primary">View
-                    Documenation</a>
             </div>
-            <div class="col-md-6">
-                <img src="https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvbnRhY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-                    alt="" class="img-fluid">
+            <div class="col-md-3">
+                <img src="https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="" class="img-fluid img-thumbnail">
+                <h3 class="text-center">John Doe</h3>
+                <p class="text-center">CEO</p>
+            </div>
+
+            <div class="col-md-3">
+                <img src="https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="" class="img-fluid img-thumbnail">
+                <h3 class="text-center">John Doe</h3>
+                <p class="text-center">CEO</p>
+            </div>
+
+            <div class="col-md-3">
+                <img src="https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="" class="img-fluid img-thumbnail">
+                <h3 class="text-center">John Doe</h3>
+                <p class="text-center">CEO</p>
             </div>
         </div>
-    </body>
+    </div>
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+        <!--Section description-->
+        <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us
+            directly. Our team will come back to you within
+            a matter of hours to help you.</p>
 
-    </html>
+        <div class="row m-5 shadow-lg p-3 mb-5 bg-white rounded p-5">
 
-    {{-- bootstrap js --}}
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
+            <!--Grid column-->
+            <div class="col-md-9 mb-md-0 mb-5">
+                <form id="contact-form" name="contact-form" action="{{route('home')}}" method="GET">
 
-    {{-- jquery --}}
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+                    <!--Grid row-->
+                    <div class="row">
+
+                        <!--Grid column-->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="name" name="name" class="form-control">
+                                <label for="name" class="">Your name</label>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="email" name="email" class="form-control">
+                                <label for="email" class="">Your email</label>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+
+                    </div>
+                    <!--Grid row-->
+
+                    <!--Grid row-->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="md-form mb-0">
+                                <input type="text" id="subject" name="subject" class="form-control">
+                                <label for="subject" class="">Subject</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid row-->
+
+                    <!--Grid row-->
+                    <div class="row">
+
+                        <!--Grid column-->
+                        <div class="col-md-12">
+
+                            <div class="md-form">
+                                <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                <label for="message">Your message</label>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!--Grid row-->
+
+                </form>
+
+                <div class="text-center text-md-left">
+                    <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                </div>
+                <div class="status"></div>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-3 text-center">
+                <ul class="list-unstyled mb-0">
+                    <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                        <p>Bamenda, Cameroon</p>
+                    </li>
+
+                    <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                        <p>+ 237 681 610 98</p>
+                    </li>
+
+                    <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                        <p>info@resulab.com</p>
+                    </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+
+        </div>
+
+    {{-- footer --}}
+    <footer class="bg-dark text-white mt-5 p-4 text-center">
+        <p>Resulab &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+        </p>
+    </footer>
+</body>
+
+</html>
+
+{{-- bootstrap js --}}
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+
+{{-- jquery --}}
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
 </html>
